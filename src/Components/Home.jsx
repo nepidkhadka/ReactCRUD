b107aff391327e5926eafa33dc4a94165974535e
Dipen Khadka <dipenkhadka191@gmail.com>
2023-08-30 11:25:36 +0545

    Changes to Delete Part -- Final Changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,24 +1,60 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Home = () => {
+    const navigate = useNavigate();
     const [mydata, setMyData] = useState([])
-    const [error, setError] = useState()
-    useEffect(()=>{
-            axios.get("http://localhost:3000/users")
+    const [errorandres, setErrorandres] = useState()
+    const [deleteconfirm, setdeleteconfirm] = useState(true)
+    useEffect(() => {
+        axios.get("http://localhost:3000/users")
             .then(
-                response => setMyData(response.data),
-            ).catch(err => setError(err.message))
-    }, [])
-  return (
-    <div className='d-flex flex-column justify-content-center align-items-center bg-light vh-100'>
-        <h1>Users List</h1>
-        <h2>{error}</h2>
-        <div className='w-75 rounder bg-white border shadow p-4' >
-        <div className="d-flex justify-content-end">
-            <Link to="/create" className='btn btn-lg btn-success'> Add + </Link>
-        </div>
+                response => {
+                    setMyData(response.data);
+                    // console.log(response);
+                }
+            )
+            .catch
+            (err =>{
+                
+                setErrorandres(err.message);
+                // console.log(err);
+            } )
+    }, [deleteconfirm])
+
+
+
+    const handleClick = (id) => {
+        console.log(id);
+        const confirm = window.confirm("Delete Confirm");
+        if (confirm) {
+            setdeleteconfirm(!deleteconfirm);
+            axios.delete("http://localhost:3000/users/" + id)
+                .then(res => {
+                    console.log(res);
+                    navigate('/');
+                    setErrorandres("Delete Status : " + res.statusText);
+                    setTimeout(() => {
+                        setErrorandres("");
+                    }, 2000)
+                }).catch(err => {
+                    setError(err.message);
+                    setTimeout(() => {
+                        setError("");
+                    }, 2000)
+                })
+        }
+    }
+
+    return (
+        <div className='d-flex flex-column justify-content-center align-items-center bg-light vh-100'>
+            <h1>Users List</h1>
+            <h2>{errorandres}</h2>
+            <div className='w-75 rounder bg-white border shadow p-4' >
+                <div className="d-flex justify-content-end">
+                    <Link to="/create" className='btn btn-lg btn-success'> Add + </Link>
+                </div>
                 <table className='table table-striped' >
                     <thead className='text-center' >
                         <tr>
@@ -32,7 +68,7 @@ const Home = () => {
                     </thead>
                     <tbody>
                         {
-                            mydata.map((data, i) =>(
+                            mydata.map((data, i) => (
                                 <tr key={i} >
                                     <td className='text-center' >{data.id}</td>
                                     <td className='text-center'>{data.name}</td>
@@ -41,17 +77,20 @@ const Home = () => {
                                     <td className='text-center'>{data.email}</td>
                                     <td>
                                         <Link to={`/read/${data.id}`} className='btn btn-sm btn-warning mx-2 text-center' >Read</Link>
-                                        <Link to={"/update/"+data.id} className='btn btn-sm btn-primary mx-2 text-center' >Edit</Link>
-                                        <button className='btn btn-sm btn-danger mx-2 text-center' >Delete</button>
+                                        <Link to={"/update/" + data.id} className='btn btn-sm btn-primary mx-2 text-center' >Edit</Link>
+                                        <button onClick={e => handleClick(data.id)} className='btn btn-sm btn-danger mx-2 text-center' >Delete</button>
                                     </td>
                                 </tr>
                             ))
                         }
                     </tbody>
                 </table>
+            </div>
         </div>
-    </div>
-  )
+    )
+
+
+
 }
 
 export default Home
